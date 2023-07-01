af49771332520a1eed5c24dc0b4285af9da23665
Deku1712 <tamtvnde160577@fpt.edu.vn>
2023-07-01 15:33:29 +0700

    Combining Reducers

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -32,12 +32,7 @@ class Main extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
+    
   }
 
 
@@ -58,9 +53,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
         <Home 
-        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
     />
   
       );
@@ -69,8 +64,8 @@ class Main extends Component {
     const DishWithId = () => {
       const {dishId} = useParams();
       return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(dishId,10))} />
+          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(dishId,10))[0]} 
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(dishId,10))} />
       );
     };
 
@@ -83,12 +78,12 @@ class Main extends Component {
         </Counter>
         <Routes>
           <Route path='/home' Component={HomePage} />
-          <Route exact path='/menu' element = {<Menu dishes={this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)} /> }  />
+          <Route exact path='/menu' element = {<Menu dishes={this.props.dishes} onClick = {(dishId) => this.onDishSelect(dishId)} /> }  />
           <Route path='/' element ={<Navigate to="/home" />} />
           <Route exact path='/contactus' Component={Contact} /> 
           <Route path='/menu/:dishId' Component={DishWithId} />
         </Routes>
-        <DishDetail dish= {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <DishDetail dish= {this.props.dishes.filter((dish) => dish.id === this.props.selectedDish)[0]}/>
         <Footer />
       </div>
     );
